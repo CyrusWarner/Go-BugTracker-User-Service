0dc28e802b1d4795cf9314bf841181e3c878a6ce
CyrusWarner <cyruswarner27@gmail.com>
2021-12-17 17:04:57 -0600

    Added router function to handle routes and added handlerUserRegister function.

--- main.go
+++ main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+
 	"github.com/Go-BugTracker-User-Service/db_client"
+	"github.com/Go-BugTracker-User-Service/models"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	db_client.InitializeDbConnection()
+
+	defer db_client.DBClient.Close()
+
+	router()
+}
+
+func router() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/user/register", handleUserRegister).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
+
+func handleUserRegister(w http.ResponseWriter, r *http.Request) {
+	u := models.User{}
+
+	decoder := json.NewDecoder(r.Body) // NewDecoder that returns a new decoder that reads from r
+	if err := decoder.Decode(&u); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid User Object")
+		return
+	}
+}
+
+func respondWithError(w http.ResponseWriter, statusCode int, errMessage string) {
+	respondWithJSON(w, statusCode, map[string]string{"error": errMessage})
+}
+
+func respondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
+	res, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(res)
 }
