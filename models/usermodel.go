f0568b969f694586d394fdcb193f1a1479596c4f
CyrusWarner <cyruswarner27@gmail.com>
2021-12-19 21:09:51 -0600

    Refactored query for registering a user.

--- models/usermodel.go
+++ models/usermodel.go
@@ -10,13 +10,12 @@ import (
 )
 
 type UserRegister struct {
-	UserId         int    `json:"userId"`
-	FirstName      string `json:"firstName"`
-	LastName       string `json:"lastName"`
-	Email          string `json:"email"`
-	Password       string `json:"-"` // omit the password field
-	EmailConfirmed bool   `json:",omitempty"`
-	DateJoined     string `json:",omitempty"`
+	UserId     int    `json:"userId"`
+	FirstName  string `json:"firstName"`
+	LastName   string `json:"lastName"`
+	Email      string `json:"email"`
+	Password   string `json:"-"` // omit the password field
+	DateJoined string `json:",omitempty"`
 }
 
 var ErrUserRegistered error = errors.New("user already registered")
@@ -42,7 +41,7 @@ func RegisterUser(db *sql.DB, ur UserRegister) (UserRegister, error) {
 		return ur, ErrUserRegistered
 	}
 
-	row = db.QueryRow("INSERT INTO Users (FirstName, LastName, Email, Password, DateJoined) OUTPUT INSERTED.* Values(@p1, @p2, @p3, @p4, @p5)",
+	row = db.QueryRow("INSERT INTO Users (FirstName, LastName, Email, Password, DateJoined) OUTPUT Inserted.UserId, Inserted.FirstName, Inserted.LastName, Inserted.Email, Inserted.DateJoined Values(@p1, @p2, @p3, @p4, @p5)",
 		ur.FirstName,
 		ur.LastName,
 		ur.Email,
@@ -55,8 +54,6 @@ func RegisterUser(db *sql.DB, ur UserRegister) (UserRegister, error) {
 		&ur.FirstName,
 		&ur.LastName,
 		&ur.Email,
-		&ur.Password,
-		&ur.EmailConfirmed,
 		&ur.DateJoined,
 	)
 
